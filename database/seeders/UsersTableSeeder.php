6407b8827900a7d1dc7b6677924af2921f01eb6c
devvspl <devendrakumar.vspl@gmail.com>
2026-05-14 20:22:21 +0530

    Seed users Manju and Amit with admin roles
    Added seeding for Manju and Amit accounts with admin roles.

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +14,26 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Manju account
+        User::updateOrCreate(
+            ['email' => 'manju@7xbasket.com'],
+            [
+                'name'     => 'Manju',
+                'email'    => 'manju@7xbasket.com',
+                'password' => Hash::make('Manju@123'),
+                'role'     => 'admin',
+            ]
+        );
+
+        // Amit account
+        User::updateOrCreate(
+            ['email' => 'amit@7xbasket.com'],
+            [
+                'name'     => 'Amit',
+                'email'    => 'amit@7xbasket.com',
+                'password' => Hash::make('Amit@123'),
+                'role'     => 'admin',
+            ]
+        );
     }
 }
